de2d77846245c9d5c0e19b84849c66cbf5b68daf
Brett Samblanet <brettsam@microsoft.com>
2016-01-06 15:15:57 -0800

    Adding new Swagger nuget package for creating swagger doc and ui

---
+++ samples/SampleApp/App_Start/Startup.Swagger.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Swagger;
+using Swashbuckle.Application;
+
+namespace Local
+{
+    public partial class Startup
+    {
+        public static void ConfigureSwagger(HttpConfiguration config)
+        {
+            config.Services.Replace(typeof(IApiExplorer), new MobileAppApiExplorer(config));
+            config
+               .EnableSwagger(c =>
+               {
+                   c.SingleApiVersion("v1", "brettsam1201Service");
+                   c.OperationFilter<MobileAppHeaderFilter>();
+                   c.SchemaFilter<MobileAppSchemaFilter>();
+                   c.AppServiceAuthentication("https://brettsam1201.azurewebsites.net/", "aad");
+               })
+               .EnableSwaggerUi(c =>
+               {
+                   c.EnableOAuth2Support("test-client-id", "test-realm", "Swagger UI");
+                   c.MobileAppUi();
+               });
+        }
+    }
+}
---
+++ test/Microsoft.Azure.Mobile.Server.Swagger.Test/SwaggerUiConfigExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Owin.Testing;
+using Swashbuckle.Application;
+using Xunit;
+
+namespace Microsoft.Azure.Mobile.Server.Swagger.Test
+{
+    public class SwaggerUiConfigExtensionsTests
+    {
+        [Fact]
+        public async Task SwaggerUiDocs()
+        {
+            // Arrange
+            TestServer server = SwashbuckleHelper.CreateSwaggerServer(null, c =>
+            {
+                c.MobileAppUi();
+            });
+
+            string o2cExpected = GetResourceString("Microsoft.Azure.Mobile.Server.Swagger.o2c.html");
+            string oauthExpected = GetResourceString("Microsoft.Azure.Mobile.Server.Swagger.swagger-oauth.js");
+
+            // Act
+            var o2cResponse = await server.HttpClient.GetAsync("http://localhost/swagger/ui/o2c-html");
+            var oauthResponse = await server.HttpClient.GetAsync("http://localhost/swagger/ui/lib/swagger-oauth-js");
+
+            string o2c = await o2cResponse.Content.ReadAsStringAsync();
+            string oauth = await oauthResponse.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(o2cExpected, o2c);
+            Assert.Equal(oauthExpected, oauth);
+        }
+
+        private static string GetResourceString(string resourceName)
+        {
+            string resourceText;
+
+            using (Stream stream = typeof(SwaggerUiConfigExtensions).Assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    resourceText = reader.ReadToEnd();
+                }
+            }
+
+            return resourceText;
+        }
+    }
+}
