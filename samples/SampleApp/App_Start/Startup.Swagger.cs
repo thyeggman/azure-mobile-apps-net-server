e47201facfa83658250a058f804f2e8b83f06367
Brett Samblanet <brettsam@microsoft.com>
2016-01-13 08:45:31 -0800

    addressing CR comments

--- samples/SampleApp/App_Start/Startup.Swagger.cs
+++ samples/SampleApp/App_Start/Startup.Swagger.cs
@@ -10,18 +10,34 @@ namespace Local
     {
         public static void ConfigureSwagger(HttpConfiguration config)
         {
+            // Use the custom ApiExplorer that applies constraints. This prevents
+            // duplicate routes on /api and /tables from showing in the Swagger doc.
             config.Services.Replace(typeof(IApiExplorer), new MobileAppApiExplorer(config));
             config
                .EnableSwagger(c =>
                {
-                   c.SingleApiVersion("v1", "brettsam1201Service");
+                   c.SingleApiVersion("v1", "myService");
+
+                   // Tells the Swagger doc that any MobileAppController needs a
+                   // ZUMO -API-VERSION header with default 2.0.0
                    c.OperationFilter<MobileAppHeaderFilter>();
+
+                   // Looks at attributes on properties to decide whether they are readOnly.
+                   // Right now, this only applies to the DatabaseGeneratedAttribute.
                    c.SchemaFilter<MobileAppSchemaFilter>();
-                   c.AppServiceAuthentication("https://brettsam1201.azurewebsites.net/", "aad");
+
+                   // 1. Adds an OAuth implicit flow description that points to App Service Auth with the specified provdier
+                   // 2. Adds a Swashbuckle filter that applies this Oauth description to any Action with [Authorize]
+                   c.AppServiceAuthentication("https://{mysite}.azurewebsites.net/", "{provider}");
                })
                .EnableSwaggerUi(c =>
                {
-                   c.EnableOAuth2Support("test-client-id", "test-realm", "Swagger UI");
+                   c.EnableOAuth2Support("na", "na", "na");
+
+                   // Replaces some javascript files with specific logic to:
+                   // 1. Do the OAuth flow using the App Service Auth parameters
+                   // 2. Parse the returned token
+                   // 3. Apply the token to the X-ZUMO-AUTH header
                    c.MobileAppUi();
                });
         }
--- src/Microsoft.Azure.Mobile.Server.Swagger/Swagger/swagger-oauth.js
+++ src/Microsoft.Azure.Mobile.Server.Swagger/Swagger/swagger-oauth.js
@@ -1,4 +1,15 @@
-﻿var appName;
+﻿// Copyright (c) 2013, Richard Morris
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+
+// This file is a modified version of the file at: https://github.com/domaindrivendev/Swashbuckle/blob/07f48b3c444be321bf58768353fbb37ca44fbe8a/Swashbuckle.Core/SwaggerUi/CustomAssets/swagger-oauth.js
+// Additions and changes to this file are surrounded by '<AzureMobile>' and '</AzureMobile>' comments.
+
+var appName;
 var popupMask;
 var popupDialog;
 var clientId;
@@ -156,9 +167,10 @@ function handleLogin() {
         url += '&scope=' + encodeURIComponent(scopes.join(scopeSeparator));
         url += '&state=' + encodeURIComponent(state);
 
-        // Azure App Service params
+        // <AzureMobile>
         url += '&post_login_redirect_url=' + encodeURIComponent(redirectUrl);
         url += '&session_mode=token';
+        // </AzureMobile>
 
         window.open(url);
     });
@@ -283,7 +295,9 @@ window.onOAuthComplete = function onOAuthComplete(token) {
                         }
                     }
                 });
+                // <AzureMobile>
                 window.swaggerUi.api.clientAuthorizations.add(oauth2KeyName, new SwaggerClient.ApiKeyAuthorization('X-ZUMO-AUTH', b, 'header'));
+                // </AzureMobile>
             }
         }
     }
